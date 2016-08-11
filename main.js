a16c2861b69f6534933b620944b6a545a8f637e7
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-08-11 12:30:36 +0200

    Events test 2

--- main.js
+++ main.js
@@ -10,17 +10,36 @@ http.createServer(function (request, response){
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
-var connectHandler = function connected() {
-  console.log('connection succesful');
-
-  eventEmitter.emit('data_received');
+// listener #1
+var listner1 = function listner1() {
+  console.log('listner1 executed.');
 }
-eventEmitter.on('connection', connectHandler);
 
-eventEmitter.on('data_received', function(){
-  console.log('data received succesfully');
-});
+// listener #2
+var listner2 = function listner2(){
+  console.log('listner2 executed.');
+}
 
+// Bind the connection event with the listner1 function
+eventEmitter.addListener('connection', listner1);
+
+// Bind the connection event with the listner2 function
+eventEmitter.on('connection', listner2);
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listener(s) listening to connection event");
+
+// Fire the connection event
 eventEmitter.emit('connection');
+
+//Remove the binding of listner1 function
+eventEmitter.removeListener('connection', listner1);
+console.log("Listner1 will not listen now.");
+
+// Fire the connection event
+eventEmitter.emit('connection');
+
+var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listener(s) listening to connection event");
 
 console.log("Program Ended");
